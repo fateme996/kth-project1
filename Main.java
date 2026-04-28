a1750b85a4d5e5c0596a55f14d1dc79610944655
Ada Björnebro <mbjorne@kth.se>
2026-04-28 15:23:26 +0200

    Add Java Swing example

--- Main.java
+++ Main.java
@@ -1,6 +1,15 @@
-public class Main {
+import javax.swing.*;
+
+public class Main extends JFrame {
+    public Main() {
+        super("Hello World");
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        add(new JLabel("Hello, World!"));
+        pack();
+        setVisible(true);
+    }
     public static void main(String[] args) {
         System.out.println("Hello Connect 4!");
-
+        SwingUtilities.invokeLater(Main::new);
     }
 }
