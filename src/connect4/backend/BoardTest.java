1450d396d435fe0356dbb1a065806e48abe8d8fd
Ada <mbjorne@kth.se>
2026-06-02 16:03:21 +0200

    Add BoardTest class

--- src/connect4/backend/BoardTest.java
+++ src/connect4/backend/BoardTest.java
@@ -1,2 +1,23 @@
-package src.connect4.backend;public class BoardTest {
+package src.connect4.backend;
+
+import org.junit.Test;
+import org.junit.Before;
+import static org.junit.Assert.*;
+
+public class BoardTest {
+    private Board board;
+    private int[][] grid;
+
+    @Before
+    public void setUp() {
+        this.board = new Board();
+        this.grid = new int[6][7];
+    }
+
+    @Test
+    public void canAddToken() {
+        board.makeMove(4);
+        this.grid[0][4] = 1;
+        assertArrayEquals(this.grid, board.getGrid());
+    }
 }
